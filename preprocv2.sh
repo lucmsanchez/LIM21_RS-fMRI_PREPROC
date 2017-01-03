323a52dd69a7aefe76f28f2d98b8b249fe9dc641
lucmsanchez <lucmsanchez@gmail.com>
2017-01-03 09:39:48 -0200

    tentativa mal sucecedida de adaptar script

--- preprocv2.sh
+++ preprocv2.sh
@@ -4,7 +4,7 @@
 # creation date: Tue Nov 29 09:50:08 2016
 
 # set data directories
-set top_dir   = /home/brain/Desktop/PROJETO_CIRCOS/PREPROCESSING/DATA/
+set top_dir   = /media/sf_SHARED/PROJETO_CIRCOS/PREPROCESSING/DATA
 
 # set subject and group identifiers
 set subj      = C000917
@@ -15,34 +15,41 @@ afni_proc.py -subj_id $subj                                        \
         -script proc.$subj -scr_overwrite                          \
         -check_afni_version yes                                    \
         -check_results_dir yes                                     \
-        -blocks tshift volreg                                      \
+        -blocks despike tshift align tlrc volreg blur mask regress \                           \
         -copy_anat $top_dir/$subj/T1_${subj}.nii                   \
-        -dsets $top_dir/$subj/RS_T000328.nii                       \
+        -dsets $top_dir/$subj/RS_${subj}.nii                       \
         -test_for_dsets yes                                        \
-        -outlier_count yes                                         \ #QC
-        -radial_correlate yes                                      \ #QC
+        -outlier_count yes                                         \
         -anat_has_skull no                                         \
-        #RICOR
-                -ricor_regress_method per-run                      \
-                -ricor_regress_solver OLSQ                         \
-                -ricor_regs REG1                                   \
-        #tshift
+        -anat_uniform_method unifize                               \
+        #tshift \
                 -tshift_interp -Fourier                            \
                 -tshift_opts_ts -tpattern seq+z                    \
-        #volreg
+        #align \
+                -align_opts_aea -giant_move                        \
+        #trlc \
+                -tlrc_base MNI_avg152T1+tlrc                       \
+                -tlrc_opts_at -init_xform AUTO_CENTER              \
+                -tlrc_NL_warp                                      \
+        #volreg \
                 -volreg_interp -Fourier                            \
-                -volreg_opts_vr -twopass -base 100 -1Dfile mc.${subj}.1d \
+                -volreg_opts_vr -twopass -base 100                 \
                 -volreg_zpad 2                                     \
-        -anat_uniform_method unifaze                               \
-        -tlrc_base MNI_avg152T1+tlrc                               \ 
-        -volreg_align_to third                                     \
-        -volreg_align_e2a                                          \
-        -volreg_tlrc_warp                                          \
-        -blur_size 4.0                                             \
-        -regress_censor_motion 0.2                                 \
-        -regress_bandpass 0.01 0.1                                 \
-        -regress_apply_mot_types demean deriv                      \
-        -regress_est_blur_errts                                    \
-        -regress_run_clustsim no                                   \
+                -volreg_align_to first                             \
+                -volreg_tlrc_warp                                  \
+                -volreg_align_e2a                                  \
+        #blur \
+                -blur_size 6.0                                     \
+        #mask \
+                -mask_segment_anat yes                             \
+                -mask_segment_erode yes                            \
+        #regress \
+                -regress_censor_motion 0.2                          \
+                -regress_censor_outliers 0.1                        \
+                -regress_bandpass 0.01 0.08                         \
+                -regress_apply_mot_types basic                      \
+                -regress_ROI WMe                                    \
+                -regress_est_blur_errts                             \
+                -regress_run_clustsim no                            \
         -bash
 

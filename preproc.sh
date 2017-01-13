272552c164e3bdb5edbdcc5defe3e1d192e06eee
lucmsanchez <lucmsanchez@gmail.com>
2017-01-13 14:28:04 -0200

    QC4 implementando, testando...

--- preproc.sh
+++ preproc.sh
@@ -592,6 +592,7 @@ mv rename.report.${ID[j]}.html report.${ID[j]}.html
 
 fi; qc.close
 done
+input.error
 echo
 
 #: QC2 ========================================================================
@@ -655,8 +656,7 @@ mv rename.report.${ID[j]}.html report.${ID[j]}.html
 
 fi; qc.close
 done
-
-exit
+input.error
 
 #: AZTEC ========================================================================
 if [ $aztec -eq 1 ]; then
@@ -708,7 +708,7 @@ echo
 printf "\n=========================MOTION CORRECTION=======================\n\n"
 for j in ${!ID[@]}; do
   inputs "${out[$j]}"
-  outputs "volreg.RS.${ID[j]}+orig" "mc.${ID[j]}.1d" "mcplot.${ID[j]}.jpg"
+  outputs "volreg.RS.${ID[j]}+orig" "mc.${ID[j]}.1d"
   echo -n "${ID[j]}> "
   if open.node "MOTION CORRECTION"; then
    ( 3dvolreg \
@@ -718,19 +718,44 @@ for j in ${!ID[@]}; do
     -twopass \
     -Fourier \
     -1Dfile ${out_2[$j]} \
-    ${in[$j]} && \
-    1dplot \
-    -jpg ${out_3[$j]} \
-    -volreg -dx $TR \
-    -xlabel Time \
-    -thick \
-    ${out_2[$j]} ) &>> preproc.${ID[j]}.log
+    ${in[$j]}  ) &>> preproc.${ID[j]}.log
   fi; close.node
 done
 input.error
 echo
 
 #: QC3 ========================================================================
+printf "=============================QC 3==================================\n\n"
+for j in ${!ID[@]}; do
+qc.open -e "QC 3"                                    \
+        -i "T1.${ID[j]}.nii"      \
+        -o "m.mcplot.${ID[j]}.jpg"              
+if [ $? -eq 0 ]; then
+ 
+   ( 1dplot \
+    -jpg "m.mcplot.${ID[j]}.jpg" \
+    -volreg -dx $TR \
+    -xlabel Time \
+    -thick \
+    ${out_2[$j]} ) &>> preproc.${ID[j]}.log
+
+read -r -d '' textf <<EOF
+<h2>QC3 - RS Motion Correction</h2>
+<p>&nbsp;</p>
+<h3>Gráfico de Correções realizadas pelo volreg</h3>
+<p><img src="m.mcplot.${ID[j]}.jpg" alt=""/></p>
+<p>&nbsp;</p>
+<hr>
+<p>&nbsp;</p>
+EOF
+
+export textf
+perl -pe 'BEGIN{undef $/;} s/<!--QC3-->.*<!--QC4-->/<!--QC3-->\n $ENV{textf} \n<!--QC4-->/smg' report.${ID[j]}.html > rename.report.${ID[j]}.html
+mv rename.report.${ID[j]}.html report.${ID[j]}.html
+
+fi; qc.close
+done
+input.error
 
 [ $break -eq 1 ] && echo "Interrompendo script a pedido do usuário" && exit
 
@@ -872,8 +897,9 @@ if [ $bet -eq 0 ]; then
   for j in ${!ID[@]}; do
     inputs "${out[$j]}"
     outputs "mask.T1.${ID[j]}.nii.gz"
+    3dAFNItoNIFTI ${in[$j]} unifize.T1.${ID[j]}.nii
     [ ! -d "$pwd/OUTPUT/${ID[j]}/manual_skullstrip" ] && mkdir -p $pwd/OUTPUT/${ID[j]}/manual_skullstrip
-    cp DATA/${ID[j]}/${ID[j]}.results/${in[$j]} OUTPUT/${ID[j]}/manual_skullstrip 2> /dev/null
+    cp DATA/${ID[j]}/${ID[j]}.results/unifize.T1.${ID[j]}.nii OUTPUT/${ID[j]}/manual_skullstrip 2> /dev/null
     ss=$(find . -name "mask.T1.${ID[j]}*")
     mv $ss ./DATA/${ID[j]}/${ID[j]}.results 2> /dev/null
   done
@@ -903,7 +929,68 @@ else
   echo
 fi 
 
-#: QC5 ========================================================================
+#: QC4 ========================================================================
+printf "=============================QC 4==================================\n\n"
+for j in ${!ID[@]}; do
+qc.open -e "QC 4"                                    \
+        -i "unifize.T1.${ID[j]}.nii"      \
+        -o "m.over.SS.T1.${ID[j]}.png m.overz.T1.${ID[j]}.mp4 m.overy.T1.${ID[j]}.mp4 m.overx.T1.${ID[j]}.mp4"              
+if [ $? -eq 0 ]; then
+
+( 3dAFNItoNIFTI unifize.T1.${ID[j]}+orig unifize.T1.${ID[j]}.nii
+  fsl5.0-overlay 1 0 -c unifize.T1.${ID[j]}.nii -a mask.T1.${ID[j]}.nii.gz 0 1 over.SS.T1.${ID[j]}
+
+  m=0
+  for n in $(seq 0.10 0.01 0.90); do
+  m=$((m + 1))
+  fsl5.0-slicer over.SS.T1.${ID[j]}.nii.gz -s 2 -x $n slicex-$m.png -y $n slicey-$m.png -z $n slicez-$m.png 
+  done
+  for q in x y z;do
+  avconv -f image2 -y -i slice$q-%d.png -filter:v "setpts=10*PTS" -r 20 m.over$q.T1.${ID[j]}.mp4
+  rm slice$q*
+  done
+
+for n in 0.15 0.20 0.25 0.30 0.35 0.40 0.45 0.50 0.55 0.60 0.65 0.70 0.75 0.80 0.85; do
+fsl5.0-slicer unifize.T1.${ID[j]}.nii mask.T1.${ID[j]}.nii.gz -s 2 -x $n im.x-$n.png -y $n im.y-$n.png -z $n im.z-$n.png 
+done
+
+  convert -append im.x*.png imx.SS.T1.${ID[j]}.png
+  convert -append im.y*.png imy.SS.T1.${ID[j]}.png
+  convert -append im.z*.png imz.SS.T1.${ID[j]}.png
+  convert +append imx.* imy.* imz.* m.over.SS.T1.${ID[j]}.png
+ 
+rm im* ) &>> preproc.${ID[j]}.log 
+
+read -r -d '' textf <<EOF
+<h2>QC3 - T1 vs. SS mask</h2>
+<p>&nbsp;</p>
+<h3>Vídeo axial</h3>
+<p><video controls="controls" width="100%" height="100%">
+<source src="m.overz.T1.${ID[j]}.mp4" /></video></p>
+<p>&nbsp;</p>
+<h3>Vídeo sagital</h3>
+<p><video controls="controls" width="100%" height="100%">
+<source src="m.overy.T1.${ID[j]}.mp4" /></video></p>
+<p>&nbsp;</p>
+<h3>Vídeo coronal</h3>
+<p><video controls="controls" width="100%" height="100%">
+<source src="m.overx.T1.${ID[j]}.mp4" /></video></p>
+<p>&nbsp;</p>
+<h3>Imagens de 3 cortes</h3>
+<p><img src="m.over.SS.T1.${ID[j]}.png" alt="" style="width:1000px;height:800px%"/></p>
+<p>&nbsp;</p>
+<hr>
+EOF
+
+export textf
+perl -pe 'BEGIN{undef $/;} s/<!--QC3-->.*<!--QC4-->/<!--QC3-->\n $ENV{textf} \n<!--QC4-->/smg' report.${ID[j]}.html > rename.report.${ID[j]}.html
+mv rename.report.${ID[j]}.html report.${ID[j]}.html
+
+fi; qc.close
+done
+input.error
+
+exit
 
 [ $break -eq 2 ] && echo "Interrompendo script a pedido do usuário" && exit
 
